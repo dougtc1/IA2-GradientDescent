afb6371cccb1898b6dc8cfc4c74bf72c7ff434ec
Douglas Torres <douglastorres.usb@gmail.com>
2017-02-17 11:19:00 -0400

    Avance

--- proyecto1.py
+++ proyecto1.py
@@ -7,6 +7,8 @@ def normalize(data):
 	for attr in data.values():
 		media = mean(attr)
 		desviacion = pstdev(attr,media)
+		#print(media)
+		#print(desviacion)
 		for x in range(0,len(attr)):
 			attr[x] = (attr[x] - media) / desviacion
 
@@ -26,27 +28,50 @@ def loadCsv(filename):
 					lista[i].append(float(row[i]))
 
 	for x in range(0,len(lista)):
-		lista[i] = np.array(lista[i])
+		print('eu')
+		lista[x] = np.array(lista[x])
 			
 	return lista
 
-def cost(alpha, thetasVector, valueVector,predictionVector):
+def cost(alpha, thetasVector, valueVector,predictionVector,repeticiones):
 	n = len(thetasVector)
-
-	for i in range(0,25):
+	#restar = 0
+	#print(valueVector)
+	#print(thetasVector)
+	for i in range(0,repeticiones):
 		for x in range(0,n):
-			thetasVector[x] = thetasVector[x] - alpha * (1 / n) * sum(((thetasVector * valueVector) - predictionVector[x])*valueVector[x])
+			restar = 0 
+			for y in range(len(valueVector)):
+				if x == 1:
+					restar += ((thetasVector[0] + thetasVector[1] * valueVector[y]) - predictionVector[y])*valueVector[y]
+				else:
+					restar += (thetasVector[0] + thetasVector[1] * valueVector[y]) - predictionVector[y]
+			#print(restar)
+			#print(x)
+			#print((restar*0.1)/alpha)
+			thetasVector[x] -= alpha * (1 / n) * restar
+	#print(thetasVector)
 
-	print(thetasVector)
 
 # "Main"
+numIteraciones = int(input("Ingrese el número de iteraciones: "))
 alpha = 0.1
 lista = loadCsv('x01.csv')
-print(lista)
 normalize(lista)
-thetas = np.zeros(len(lista[0]))
-cost(alpha, thetas, lista[0], lista[1])
+#print(lista)
+#print(lista)
+thetas = np.zeros(len(lista))
+cost(alpha, thetas, lista[0], lista[1], numIteraciones)
+#print(thetas)
+cosa = (thetas[0]+1.620*thetas[1])
+print(cosa)
+##med = mean(lista[1])
+#esv = pstdev(lista[1])
+#print(cosa*desv+med)
 #print(pstdev(lista[0]))
 #plt.scatter(lista[0], lista[1])
 #plt.show()
 #print(lista)
+#198.78998387096775
+#891.8772590272449
+
